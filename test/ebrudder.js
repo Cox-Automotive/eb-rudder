ef1c54b344a73e743ad240a775a63d203b73639e
Brian <brian.antonelli@gmail.com>
2016-10-28 13:07:02 -0400

    works!

---
+++ test/ebrudder.js
@@ -0,0 +1,11 @@
+/*jslint node: true */
+'use strict';
+
+var ebrudder = require('../bin/ebrudder');
+var assert   = require('chai').assert;
+
+describe('ebrudder', function(){
+    it('should write some tests', function(done){
+        done();
+    });
+});
